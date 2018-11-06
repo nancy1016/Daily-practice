e20bd72adec8c65251fd62350f0f2f4e23f516a0
nancy1016 <liukaixa@163.com>
2018-11-06 16:40:09 +0800

    strstr函数完善

--- test_11_3/test_11_3/test.c
+++ test_11_3/test_11_3/test.c
@@ -71,36 +71,115 @@
 //	return dest;
 //}
 
-void Reverse(char*left, char*right)
+
+
+
+//左旋字符串
+//void Reverse(char*left, char*right)
+//{
+//	while (left < right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//void Reverse_K(char*str, int k)
+//{
+//	int len = strlen(str);
+//	Reverse(str, str+len-1);//整体翻转
+//	Reverse(str, str+len-1-k);//再翻转前半部分
+//	Reverse(str+len-k,str+len-1);//翻转后半部分	
+//}
+
+
+//我的
+char*Strstr(const char*str, const char*sub)
 {
-	while (left < right)
+	assert(str);
+	assert(sub);
+	const char*str_p = str;//sub_p指针遍历源字符串进行比较
+	const char*start = str;//start指针保存匹配成功的位置
+	const char*sub_p = sub;//sub_p遍历子串进行比较
+	while (*start != '\0')
 	{
-		char tmp = *left;
-		*left = *right;
-		*right = tmp;
-		left++;
-		right--;
+		str_p = start;
+		sub_p = sub;//每次匹配不成功时都要从子串的起始处重新比较
+		while (*str_p != '\0'&&*sub_p != '\0')
+		{
+			if (*str_p == *sub_p)
+			{
+				str_p++;
+				sub_p++;
+			}
+			else
+			{
+				break;
+			}
+		}
+		if (*sub_p == '\0')
+		{
+			return start;
+		}
+		start++;//当前位置开始匹配不成功时，从下一个位置开始尝试匹配
 	}
+	return NULL;
 }
 
-void Reverse_K(char*str, int k)
-{
-	int len = strlen(str);
-	Reverse(str, str+len-1);
-	Reverse(str, str+len-1-k);
-	Reverse(str+len-k,str+len-1);	
-}
+
+//char*Strstr(const char*str, const char*sub)
+//{
+//		assert(str);
+//		assert(sub);
+//		const char*str_p = str;//str_p真正向后走
+//		const char*start = str_p;//start用来指向起始位置
+//		const char*sub_p = sub;
+//		while (*start)//外层循环只是决定了从什么时候开始
+//		{
+//			str_p = start;
+//			sub_p = sub;
+//			//内层循环才是真正比较
+//			while (*str_p&&*sub_p&&*str_p == *sub_p)
+//			{
+//				str_p++;
+//				sub_p++;
+//			}
+//			//只要两个字符不相等就停下来，或者有一个已经到达\0就停下来
+//			//*sub_p=='\0'说明比较成功了，这时候返回起始比较位置，而起始比较位置在start当中保存着
+//			if (*sub_p == '\0')
+//			{
+//				return start;
+//			}
+//			start++;
+//		}
+//		return NULL;
+//}
 int main()
 {
-	char str1[] = "ABCD";
-	Reverse_K(str1, 2);
-	printf("%s", str1);
+
+	char str[] = "abbcd";
+	char *p;
+	p = Strstr(str, "bc");
+	printf("%s", p);
+
+
+	/*char str1[] = "ABCD";
+	int k = 0;
+	scanf("%d", &k);
+	Reverse_K(str1, k);
+	printf("%s", str1);*/
+
+
+
 	/*int arr1[10] = { 0 };
 	int arr2[5] = { 1, 2, 3, 4, 5 };
 	Memcpy(arr1, arr2, sizeof(arr2));
 	for (int i = 0; i < sizeof(arr1)/sizeof(arr1[0]); i++)
 	{
-		printf("%d ", arr1[i]);
+	printf("%d ", arr1[i]);
 	}*/
 
 
